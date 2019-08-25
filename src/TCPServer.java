6651e13775d972725f8d9c2a02fce3b060c92ea4
Dion <dion.balmforth@gmail.com>
2019-08-25 23:04:33 +1200

    adding pseudo to files

---
+++ src/TCPServer.java
@@ -0,0 +1,31 @@
+//SFTP class
+
+//USER command
+
+//ACCT command
+
+//PASS command
+
+//TYPE command
+
+//LIST command
+
+//CDIR command
+
+//KILL command
+
+//NAME command
+
+//DONE command
+
+//RETR command
+
+//STOR command
+
+//TOBE command - what dis
+
+//SEND command - what dis
+
+//STOP command - what dis
+
+//SIZE command - what dis
